45a6c7e1bde84f54366638ff62c191a6ee843322
ilenych <ilenych6@gmail.com>
2020-04-14 17:06:15 +0500

    refactoring code

--- mainPage/js/post.js
+++ mainPage/js/post.js
@@ -1,25 +1,27 @@
+// Future function
 
-
-
+// create elements
 function getStackElements(text) {
   return document.querySelectorAll(text);
 }
-
+// create addEventListener
 function addEventListenerClick(text, func) {
   let element = getStackElements(text);
   element.forEach((el) => {
     el.addEventListener("click", func);
   });
 }
-
+// created addEventListener
 addEventListenerClick(".like_smiles", prepare);
 addEventListenerClick(".sender__send", send);
 
+//Preparee and curreent Target
 function prepare() {
   let element = event.currentTarget;
   likeIsSelected(element.dataset.id);
 }
 
+// If like is selected(text greend and font size if more)
 function likeIsSelected(id) {
   let element = getStackElements(".like_smiles__text");
   element[id].classList.add("likeIsSelected");
@@ -27,10 +29,12 @@ function likeIsSelected(id) {
   element[id].innerHTML = plusOneLike(element[id].innerHTML);
 }
 
+//Plus one loke to current text
 function plusOneLike(text) {
   return Number(text) + 1; // true false
 }
 
+//Remove event
 //   function removeEventListenerToHover(text, func) {
 //     let element = getStackElements(text);
 //     element.forEach((el) => {
@@ -39,6 +43,7 @@ function plusOneLike(text) {
 //   }
 //********************************************** */
 
+// Copy text in textarea and push render commnet
 function send() {
   const element = getStackElements(".sender__textarea");
   let text = "";
